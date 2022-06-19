68c9382faf7c12c4a2dd8f527b15a2a593cbff47
sina-byn <sina.sina.bayan@protonmail.com>
2022-06-19 11:54:11 +0430

    main: setNotesCount + DeleteNote Functionality Added

--- js/script.js
+++ js/script.js
@@ -6,6 +6,9 @@ const cancelNoteBtn = document.querySelector(".cancel-note-btn");
 const newNoteSection = document.querySelector(".new-note-section");
 const newTitleInp = newNoteSection.querySelector("div input");
 const newNoteInp = newNoteSection.querySelector("textarea");
+const deleteBtns = document.querySelectorAll(".delete-note-btn");
+
+let notesCount = 1;
 
 // Functions
 const showNewTextField = () => {
@@ -21,15 +24,16 @@ const addNote = () => {
     const isFieldShown = Array.from(newNoteSection.classList).indexOf("flex");
 
     if (isFieldShown && isValueValid) {
-        console.log(title);
-        console.log(text);
+        addNoteCard(title, text);
+        setNotesCount();
+        newNoteSection.classList.replace("flex", "hidden");
     }
 };
 
 const resetNote = () => {
     const currentTitleInp = newTitleInp;
     const currentNoteInp = newNoteInp;
-    
+
     currentTitleInp.value = "";
     currentNoteInp.value = "";
 };
@@ -44,8 +48,52 @@ const cancelNote = () => {
     currentNoteInp.value = "";
 };
 
+const addNoteCard = (title, text) => {
+    const notesList = document.querySelector(".notes-list");
+    const noteCard = document.createElement("div");
+    const cardData = `
+        <section>
+            <p
+                class="flex items-center justify-center w-5 h-5 text-xs border-px border-gray-400 rounded-full mb-2">
+                ${++notesCount}
+            </p>
+            <div>
+                <p class="title text-gray-700">${title}</p>
+                <p class="text text-sm text-secondary-dark">
+                  ${text}
+                </p>
+            </div>
+        </section>
+        <section class="flex flex-col items-center justify-center gap-y-2 text-secondary-dark">
+            <i class="fa-solid fa-external-link cursor-pointer hover:text-gray-600"></i>
+            <i class="fa-solid fa-edit cursor-pointer hover:text-gray-600"></i>
+            <i class="fa-solid fa-trash cursor-pointer hover:text-gray-600"></i>
+        </section>
+    `;
+
+    noteCard.className = "card flex justify-between border-b-px border-secondary border-opacity-75 py-3 px-3";
+    noteCard.innerHTML = cardData;
+    notesList.append(noteCard);
+};
+
+const setNotesCount = () => {
+    const notesCountDisp = document.querySelector(".notes-count-disp");
+    notesCountDisp.innerText = notesCount + " notes";
+};
+
+const deleteNote = (e) => {
+    const targetNoteCard = e.target.parentElement.parentElement;
+    targetNoteCard.remove();
+    notesCount--;
+    setNotesCount();
+};
+
 // EventListeners
+window.onload = setNotesCount;
 newNoteBtn.onclick = showNewTextField;
 addNoteBtn.onclick = addNote;
 resetNoteBtn.onclick = resetNote;
 cancelNoteBtn.onclick = cancelNote;
+deleteBtns.forEach(btn => {
+    btn.onclick = deleteNote;
+});
