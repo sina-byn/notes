648a0459ea9f4a11257f3aeff415cdde9c941592
sina-byn <sina.sina.bayan@protonmail.com>
2022-08-29 18:26:46 +0430

    main: notes list responsive issues fixed + other responsive changes

--- js/app.js
+++ js/app.js
@@ -10,13 +10,15 @@ const toggleMenuVisibility = (sidebar, button) => {
         button.children[1].style.display = "none";
         button.classList.replace('left-0', 'left-[240px]');
         sidebar.classList.replace('-translate-x-full', 'translate-x-0');
+        sidebar.style.zIndex = '100';
         return;
     }
-
+    
     button.children[0].style.display = "none";
     button.children[1].style.display = "initial";
     button.classList.replace('left-[240px]', 'left-0');
     sidebar.classList.replace('translate-x-0', '-translate-x-full');
+    sidebar.style.zIndex = '10';
 };
 
 menuIcon.onclick = toggleMenuVisibility.bind(null, sideMenu, menuIcon);
